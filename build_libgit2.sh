8cfbb9bf92b7e6e955359a2869c412c7b1ed511a
Dave Carlton <davec@polymicrosystems.com>
2011-03-16 20:21:43 -0400

    Modified to compile only i386 and x86_64 for Xcode 4.

--- build_libgit2.sh
+++ build_libgit2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh 
 
 # build_libgit2.sh
 # GitX
@@ -9,6 +9,7 @@
 # based on: http://log.yeahrightkeller.com/post/270155578/run-script-while-cleaning-in-xcode
 
 buildAction () {
+	set -x
     echo "Building libgit2..."
 	if [[ -d .git ]]
 	then
@@ -17,7 +18,7 @@ buildAction () {
 		git submodule update
 		cd libgit2
 		rm -f libgit2.a
-		make CFLAGS="-arch i386 -arch ppc -arch x86_64"
+		make CFLAGS="-arch i386 -arch x86_64"
 		ranlib libgit2.a
 	else
 		echo "error: Not a git repository."
